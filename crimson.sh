8fc83ae9b1eb45b41f706e9e5d89f1111c0fd46a
Rob King <rking188@gmail.com>
2018-07-19 23:35:01 -0400

    Update hashes in crimson script

--- crimson.sh
+++ crimson.sh
@@ -5,9 +5,12 @@
 # Positive Outlook: 3393130645
 # Rat King Sparrow: 1173626681
 # Rat King sidearm: 2362471601
+# Jade Rabbit (Jester ornament)
 #hash="1173626681"
+#hash="3393130645"
 #hash="3437746471"
 hash="2362471601"
+#hash="1970437989"
 #rm output/gear.scnassets/$hash/$hash.dae
 rm output/gear.scnassets/$hash/$hash.usda
 rm output/gear.scnassets/$hash/$hash.usdc
