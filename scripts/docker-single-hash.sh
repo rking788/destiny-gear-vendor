8ea1bb92397d42e3475a1ea101a51444b3b39f62
Rob <rking188@gmail.com>
2020-09-12 21:48:40 -0400

    Update the single docker command script to use the latest image version

--- scripts/docker-single-hash.sh
+++ scripts/docker-single-hash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -it -v /Users/rking/dev/go/src/github.com/rking788/destiny-gear-vendor/docker-gear.scnassets:/root/output/gear.scnassets -e PORT=8181 -p 8181:8181 -p 55432:55432  -e DATABASE_URL=$DATABASE_URL -e BUNGIE_API_KEY=$BUNGIE_API_KEY registry.gitlab.com/rpk788/destiny-gear-vendor:1.0.0-baff123 ./server --cli --geom --textures --usdz --hash 821154603 
+docker run -it -v /Users/rking/dev/go/src/github.com/rking788/destiny-gear-vendor/docker-gear.scnassets:/root/output/gear.scnassets -e PORT=8181 -p 8181:8181 -p 55432:55432  -e DATABASE_URL=$DATABASE_URL -e BUNGIE_API_KEY=$BUNGIE_API_KEY registry.gitlab.com/rpk788/destiny-gear-vendor ./server --cli --geom --textures --usdz --hash 821154603 
 
